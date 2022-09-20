e4ffa7f47c6134eb82de9330169531fefa8a6066
GuilleSantaCruz <skrguille@gmail.com>
2022-09-20 17:53:46 -0300

    Homework HTML completado!

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -22,7 +22,8 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  cb(n1,n2);
+  console.log( cb(n1,n2));
+
 
 }
 
